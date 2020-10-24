720b9e550c3d1cd3247b699dad9e8e3757bdb91c
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-24 17:02:34 -0300

    Cleaned up the startup file

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AdvisorRegistrationWebApi.Models.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AdvisorRegistrationWebApi
 {
@@ -23,7 +17,6 @@ namespace AdvisorRegistrationWebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -32,7 +25,6 @@ namespace AdvisorRegistrationWebApi
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
