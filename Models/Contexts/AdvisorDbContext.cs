15a4be764c9eba4d057eb0ac6d796e2dec4c1dde
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-24 13:07:57 -0300

    Added AdvisorDbContext

---
+++ Models/Contexts/AdvisorDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdvisorRegistrationWebApi.Models.Contexts
+{
+    public class AdvisorDbContext:DbContext
+    {
+        public AdvisorDbContext(DbContextOptions<AdvisorDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Advisor> Advisors { get; set; }
+    }
+}
