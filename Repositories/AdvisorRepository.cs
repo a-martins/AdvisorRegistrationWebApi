6d8511d887e7d3f87b1b2dedb3be3db1e6d31dbf
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-24 18:07:14 -0300

    Added AdvisorRepository Pattern

---
+++ Repositories/AdvisorRepository.cs
@@ -0,0 +1,77 @@
+﻿using AdvisorRegistrationWebApi.Models;
+using AdvisorRegistrationWebApi.Models.Contexts;
+using AdvisorRegistrationWebApi.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvisorRegistrationWebApi.Repositories
+{
+    public class AdvisorRepository : IAdvisorRepository
+    {
+        private readonly AdvisorDbContext _context;
+        private bool disposed = false;
+
+        public AdvisorRepository(AdvisorDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Advisor>> GetAdvisorsAsync()
+        {
+            return await _context.Advisors.ToListAsync();
+        }
+
+        public async Task<Advisor> GetAdvisorByIDAsync(int advisorId)
+        {
+            return await _context.Advisors.FindAsync(advisorId);
+        }
+
+        public bool Any(int advisorId)
+        {
+            return _context.Advisors.Any(e => e.id == advisorId);
+        }
+
+        public void InsertAdvisor(Advisor advisor)
+        {
+            _context.Advisors.Add(advisor);
+        }
+
+        public void UpdateAdvisor(Advisor advisor)
+        {
+            _context.Entry(advisor).State = EntityState.Modified;
+        }
+
+        public void DeleteAdvisor(int advisorId)
+        {
+            Advisor advisor = _context.Advisors.Find(advisorId);
+            _context.Advisors.Remove(advisor);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+    }
+}
---
+++ Repositories/Interface/IAdvisorRepository.cs
@@ -0,0 +1,18 @@
+﻿using AdvisorRegistrationWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdvisorRegistrationWebApi.Repositories.Interface
+{
+    public interface IAdvisorRepository : IDisposable
+    {
+        Task<IEnumerable<Advisor>> GetAdvisorsAsync();
+        Task<Advisor> GetAdvisorByIDAsync(int advisorId);
+        void InsertAdvisor(Advisor advisor);
+        void DeleteAdvisor(int advisorId);
+        void UpdateAdvisor(Advisor advisor);
+        bool Any(int advisorId);
+        Task SaveAsync();
+    }
+}
