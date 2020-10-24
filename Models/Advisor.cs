10290bcc95aa7c573afd5202b415bd2cec0c8793
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-24 13:04:36 -0300

    Added Advisor Model

---
+++ Models/Advisor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdvisorRegistrationWebApi.Models
+{
+    public class Advisor
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName="nvarchar(100)")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string mobile { get; set; }
+
+        public int registryNumber { get; set; }
+
+        public int memberSince { get; set; }
+
+        public bool status { get; set; }
+    }
+}
