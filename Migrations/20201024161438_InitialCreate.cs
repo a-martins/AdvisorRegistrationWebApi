015c42dc3a1e7068078af2152504682079095128
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-24 13:15:41 -0300

    Added Advisor Migration

---
+++ Migrations/20201024161438_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvisorRegistrationWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Advisors",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fullName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    mobile = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    registryNumber = table.Column<int>(nullable: false),
+                    memberSince = table.Column<int>(nullable: false),
+                    status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advisors", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Advisors");
+        }
+    }
+}
